4b4e5826b9db77128c5f162d673174477f6eccc6
erickgfs <erickgfs@gmail.com>
2020-08-26 18:54:37 -0300

    Fundamentos NodeJS finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import api from './services/api';
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
-import './App.css';
+import "./App.css";
 
-import Header from './components/Header'
+import Header from "./components/Header";
 
 function App() {
-    const [projects, setProjects] = useState([]); //use state = retorna array com 2 posições (1 = variavel com valor inicial, 2 = function para atualizar valor)
+  const [projects, setProjects] = useState([]); //use state = retorna array com 2 posições (1 = variavel com valor inicial, 2 = function para atualizar valor)
 
-    useEffect(() => {
-        api.get('projects').then(response => {
-            setProjects(response.data)
-        });
-    }, []);
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
-    async function handleAddProject() {
-        // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
-        const response = await api.post('projects',{
-            title: `Novo Projeto ${Date.now()}`,
-            owner: "Erick Giovanni"
-        });
+  async function handleAddProject() {
+    const response = await api.post("projects", {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: "Erick Giovanni",
+    });
 
-        const project = response.data;
+    const project = response.data;
 
-        setProjects([...projects, project]);
-    }
+    setProjects([...projects, project]);
+  }
 
-    return (
-        <>
-            <Header title="Projects" />
+  return (
+    <>
+      <Header title="Projects" />
 
-            <ul>
-                {projects.map(project => <li key={project.id}>{project.title}</li>)}
-            </ul>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>{project.title}</li>
+        ))}
+      </ul>
 
-            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
-        </>
-    );
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
+    </>
+  );
 }
 
 export default App;
