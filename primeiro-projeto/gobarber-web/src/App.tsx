b84ab0a11d4ea0d0cf959f58f677b87b3c358ac9
erickgfs <erickgfs@gmail.com>
2020-10-07 14:47:37 -0300

    atualização

--- primeiro-projeto/gobarber-web/src/App.tsx
+++ primeiro-projeto/gobarber-web/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-// import SignIn from './pages/SignIn/index';
-import SignUp from './pages/SignUp/index';
+import SignIn from './pages/SignIn/index';
+// import SignUp from './pages/SignUp/index';
+
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
     <GlobalStyle />
   </>
 );
